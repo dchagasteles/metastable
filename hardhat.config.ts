303279c723efc2bfaaff2ff0898d1f2fb2423ecf
conlotor <stepcek80@gmail.com>
2022-01-26 19:06:21 +0000

    feat: bump solc to 0.8.11

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,7 +5,7 @@ import '@nomiclabs/hardhat-waffle';
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: '0.8.7',
+    version: '0.8.11',
     settings: {
       optimizer: {
         enabled: true,
